456f19d413b55f5c886d228a69fc65cc11a0764f
Øystein Sørensen <oystein_sorensen@hotmail.com>
2018-02-23 17:10:44 +0100

    Added C++ file fit_mu_lasso.cpp and made it run. 1 note in R CMD CHECK, which I have to investigate

--- R/project_gradient.R
+++ R/project_gradient.R
@@ -1,5 +1,7 @@
 # Function which does the project gradient descent
 # Based on a Matlab function developed by Po-Ling Loh and Martin J. Wainwright
+#' @useDynLib hdme, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
 project_gradient <- function(Q, b, maxits, alpha, R, betaOld) {
   its <- 1 # Number of iterations
   change <- 1 # Initial value, change per iteration
